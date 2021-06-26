5b792edbfbbb6bb2da771a4ce2f9fe1f3fd19587
root <root@instance-3.us-central1-a.c.learning1-309608.internal>
2021-06-26 07:25:18 +0000

    update build and compose

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 IP=$(hostname -I | awk -F ' ' '{print $1}')
 sed -e "s#INSTANCE_IP#$IP#g" db/init.sql.tmp > db/init.sql
 docker-compose down
-docker volume rm test_data-mysql
+#docker volume rm test_data-mysql
 docker-compose up -d
-docker-compose logs -f
+#docker-compose logs -f
