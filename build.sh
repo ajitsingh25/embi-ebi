c1220487e4fc692099a20aa6b4c2c17925854b54
root <root@instance-3.us-central1-a.c.learning1-309608.internal>
2021-06-26 08:11:01 +0000

    fix apache conf

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 IP=$(hostname -I | awk -F ' ' '{print $1}')
 sed -e "s#INSTANCE_IP#$IP#g" db/init.sql.tmp > db/init.sql
+sed -e "s#INSTANCE_NAME#$(hostname)#g" apache/000-default.conf.tmp > apache/000-default.conf
 docker-compose down
 #docker volume rm test_data-mysql
 docker-compose up -d
